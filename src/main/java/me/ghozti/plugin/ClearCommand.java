d2e6dce8e192161e3330a229d7b83fd4f0ca3aeb
ybald <ybalderas1005@gmail.com>
2021-09-11 22:24:35 -0400

    - worked on ClearCommand

--- src/main/java/me/ghozti/plugin/ClearCommand.java
+++ src/main/java/me/ghozti/plugin/ClearCommand.java
@@ -1,8 +1,11 @@
 package me.ghozti.plugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class ClearCommand implements CommandExecutor {
 
@@ -14,7 +17,19 @@ public class ClearCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+            Player player = (Player) sender;
 
+            if (player.isOp() || player.hasPermission("clearinventory")){
+                Inventory inv = player.getInventory();
+                inv.clear();
+
+                player.sendMessage(ChatColor.RED + "your inventory is clear");
+            }
+        }else {
+            main.getLogger().info("hey, console!");
+            return true;//returning true will not print an error in the console
+        }
         return false;
     }
 }
