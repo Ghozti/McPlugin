8301d3e88e6b55d1916de267892cca5cd116ad1c
ybald <ybalderas1005@gmail.com>
2021-09-11 21:55:29 -0400

    - worked on the BreakBlock class

--- src/main/java/me/ghozti/plugin/BreakBlock.java
+++ src/main/java/me/ghozti/plugin/BreakBlock.java
@@ -5,6 +5,7 @@ import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class BreakBlock implements Listener {
 
@@ -13,7 +14,10 @@ public class BreakBlock implements Listener {
         Block block = event.getBlock();
 
         if (block.getType() == Material.DIAMOND_ORE){
-
+            event.setCancelled(true);
+            block.setType(Material.WHITE_WOOL);
+            ItemStack diamonds = new ItemStack(Material.DIAMOND, 55);
+            block.getWorld().dropItemNaturally(block.getLocation(),diamonds);
         }
     }
 }
