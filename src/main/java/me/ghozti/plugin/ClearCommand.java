486727083ce9bd6118d26dc0e7a84f09f7b2539e
ybald <ybalderas1005@gmail.com>
2021-09-11 22:20:04 -0400

    - worked on ClearCommand

--- src/main/java/me/ghozti/plugin/ClearCommand.java
+++ src/main/java/me/ghozti/plugin/ClearCommand.java
@@ -1,4 +1,20 @@
 package me.ghozti.plugin;
 
-public class ClearCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ClearCommand implements CommandExecutor {
+
+    final Main main;
+
+    public ClearCommand(Main main) {
+        this.main = main;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return false;
+    }
 }
